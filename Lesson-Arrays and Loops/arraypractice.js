bf5aa9934cfb8e3a3878417bf0f864ec2b9a1960
Sushant-Luitel <sushantluitel39@gmail.com>
2023-11-10 13:24:24 +0545

    object destructuring and json api

--- Lesson-Arrays and Loops/arraypractice.js
+++ Lesson-Arrays and Loops/arraypractice.js
@@ -39,6 +39,30 @@
 // arrarFromString = Array.from("sushant");
 // console.log(arrarFromString);
 
-const myArray = [1, 2, 3, 4, 5, 6];
-slicedArray1 = myArray.slice(-4, 5);
-console.log(slicedArray1);
+// const myArray = [1, 2, 3, 4, 5, 6];
+// slicedArray1 = myArray.slice(-4, 5);
+// console.log(slicedArray1);
+
+// function lastValueArray(array) {
+//   console.log(array.pop());
+// }
+
+// lastValueArray([20, 30, 55, 65, 70]);
+
+// const myArray = [1, 2, 3, 5];
+// console.log(myArray.unshift(35));
+// console.log(myArray);
+
+// const array1 = ["sushant", "karishma"];
+// const array2 = ["luitel", "paudel", ["kaushika", "atreya"]];
+// const newArray = [...array1, ...array2.flat(3)];
+// console.log(newArray);
+
+const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(myArray.slice(3, 5));
+// console.log(myArray.splice(3, 5));
+
+// console.log(myArray.includes(0, 2));
+console.log(myArray.length);
+myArray.length = 15;
+console.log(myArray);
---
+++ Lesson-Objects/objects.js
@@ -0,0 +1,46 @@
+// const location = "pepsicola";
+// const user = {
+//   name: "sushant",
+//   age: 22,
+//   email: "sushantluitel39@gmail.com",
+//   location: "kathmandu",
+//   "current-course": "Csit",
+
+// };
+
+// console.log(user["current-course"]);
+// console.log(user["name"]);
+// user.greeting = function () {
+//   console.log(`Hello Mr. ${this.name}`);
+// };
+// user.greeting();
+const obj1 = {
+  fullname: {
+    firstName: "sushant",
+    lastName: "Luitel",
+  },
+  age: 22,
+  location: "pepsicola",
+};
+const obj2 = {
+  fullname: {
+    firstName: "sisham",
+    lastName: "Luitel",
+  },
+  age: 24,
+  location: "pepsicola",
+  father: "drona",
+};
+
+let { age } = obj2;
+
+age += 1;
+console.log(age);
+console.log(obj2);
+
+//JSON
+// {
+//     "name":'Sushant',
+//     "class":"5th sem",
+//     'age':22
+// }
